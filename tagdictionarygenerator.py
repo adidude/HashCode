e87d1c9539cd7c315630f9f7c0586903dfb94b5e
adidude <menon.or.adithya@gmail.com>
2019-02-28 21:19:54 +0000

    Update tagdictionarygenerator.py

--- tagdictionarygenerator.py
+++ tagdictionarygenerator.py
@@ -52,7 +52,21 @@ def makedictionary(diction):
           #      reform=' '
            # else:
             #    reform+=word
-                
+def outputsubmission(arr):
+
+    outputfile = open("submission.txt", "w")
+
+    systemout=sys.stdout
+
+    sys.stdout=outputfile
+
+    for x in arr:
+
+        print (x)
+
+    sys.stdout=systemout
+
+    outputfile.close()
             
                 
                 
