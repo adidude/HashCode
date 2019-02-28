c21d57789887b30acb25821c553dec8968cf679c
adidude <menon.or.adithya@gmail.com>
2019-02-28 21:21:13 +0000

    Delete outputsubmission.py

--- outputsubmission.py
+++
@@ -1,9 +0,0 @@
-def outputsubmission(arr):
-    outputfile = open("submission.txt", "w")
-    systemout=sys.stdout
-    sys.stdout=outputfile
-    for x in arr:
-        print (x)
-    sys.stdout=systemout
-    outputfile.close()
-
